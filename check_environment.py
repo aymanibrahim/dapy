4ccba868a647e3b924fde407146d1904ce0b9227
Ayman Ibrahim <31798211+aymanibrahim@users.noreply.github.com>
2019-08-08 10:57:51 +0200

    Initial commit

---
+++ check_environment.py
@@ -0,0 +1,17 @@
+import importlib
+
+packages = ['numpy', 'matplotlib', 'pandas', 'jupyterlab', 'rise']
+
+bad = []
+for package in packages:
+    try:
+        importlib.import_module(package)
+    except ImportError:
+        bad.append(f"Can't import {package}")
+else:
+    if len(bad) > 0:
+        print('Your workshop environment is not yet fully set up:')
+        print('\n'.join(bad))
+    else:
+        print("Your workshop environment is set up")
+
