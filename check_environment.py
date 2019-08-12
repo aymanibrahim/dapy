885b75378d8456682787aa2a1373c0f0ed256768
Ayman Ibrahim <31798211+aymanibrahim@users.noreply.github.com>
2019-08-12 17:34:53 +0200

    Add scikit-learn

--- check_environment.py
+++ check_environment.py
@@ -1,6 +1,6 @@
 import importlib
 
-packages = ['numpy', 'matplotlib', 'pandas', 'seaborn', 'statsmodels', 'jupyterlab', 'rise']
+packages = ['numpy', 'matplotlib', 'pandas', 'seaborn', 'statsmodels', 'sklearn', 'jupyterlab', 'rise']
 
 bad = []
 for package in packages:
